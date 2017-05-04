612a4110a7566339c70b8eb09626a81fa13c8235
dabbib <dabbib89@gmail.com>
2017-05-04 13:05:06 +0000

    teest

--- vln2Project/Controllers/ProjectController.cs
+++ vln2Project/Controllers/ProjectController.cs
@@ -8,7 +8,7 @@ namespace h37.Controllers
 {
     public class ProjectController : Controller
     {
-        private ProjectsServices _service; = new ProjectsServices();
+        private ProjectsServices _service = new ProjectsServices();
 
         // GET: Project
         public ActionResult Index()
