95345a2b6fe4ce5bf476017945e6284cc1222496
orria16 <orria16@ru.is>
2017-05-04 13:21:22 +0000

    userservices klasinn!

---
+++ vln2Project/Services/UserServices.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace h37.Services
+{
+    public class UserServices
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public int createUser(string userName, string password)
+        {
+            /* todo create new User */
+            return 0;
+        }
+        public int getUserID(int userID)
+        {
+            /* todo return UserID  */
+            return 0;
+        }
+        public int getName(string userName)
+        {
+            /* todo return User name */
+            return 0;
+        }
+        public List<project> getProjects(int userID)
+        {
+            /* return list of  projects */
+            return 0;
+        }
+    }
+}
