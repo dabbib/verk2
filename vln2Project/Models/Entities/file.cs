5aa7e2257f74c795290bef1a979ce95f00bea100
dabbib <dabbib89@gmail.com>
2017-05-04 11:37:28 +0000

    entity class file

---
+++ vln2Project/Models/Entities/file.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace h37.Models.Entities
+{
+    public class File
+    {
+        /// <summary>
+        /// Entity class for Files
+        /// </summary>
+        public int fileid { get; set; }
+        public string filename { get; set; }
+        public string filetype { get; set; }
+       
+    }
+}
