b5809e93139271b009fc1bfb1186919c1daee45d
helgib10 <helgib10@ru.is>
2017-05-04 11:12:58 +0000

    User entity class added

---
+++ vln2Project/Models/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace h37.Models.Entities
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+    }
+}
