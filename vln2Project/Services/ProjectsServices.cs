f00b6720a42b5ad779732a2c8e2da8a7299b17f0
helgib10 <helgib10@ru.is>
2017-05-05 13:10:58 +0000

    Project services documentation and style fixing.

--- vln2Project/Services/ProjectsServices.cs
+++ vln2Project/Services/ProjectsServices.cs
@@ -15,23 +15,31 @@ namespace h37.Services
         {
             db = new ApplicationDbContext();
         }
+
+        #region Project functions
+
         /// <summary>
-        /// This function creates a blank project with one file called index.js for js projects.
+        /// This function creates a new project with one file called index.js for js projects.
         /// </summary>
         /// <param name="userID">ID of the user creating the project</param>
-        /// <param name="type">Type of project</param>
-        /// <returns></returns>
+        /// <param name="projectName">Name of the project provided by user</param>
+        /// <param name="type">Type of project provided by user</param>
+        /// <returns>Id of the created project</returns>
         public int createProject(int userID, string projectName, projectType type)
         {
             Project newProject = new Project(projectName, userID, type);
+
+            /* Create the index file for the new project */
             createFile(newProject.projectID, userID, "index." + type);
+
             db.Projects.Add(newProject);
             db.SaveChanges();
 
             return newProject.projectID;
         }
+
         /// <summary>
-        /// Function to get projects by id
+        /// Function to get projects by id.
         /// </summary>
         /// <param name="projectID"></param>
         /// <returns>Single project</returns>
@@ -40,134 +48,197 @@ namespace h37.Services
             Project p = (from x in db.Projects
                          where x.projectID.Equals(projectID)
                          select x).SingleOrDefault();
-            if(p == null)
+
+            if (p == null)
             {
                 /* Todo exceptions if project does not exist */
             }
             return p;
         }
+
         /// <summary>
-        /// 
+        /// Function to get the number of files in a given project.
         /// </summary>
         /// <param name="projectID"></param>
-        /// <returns></returns>
+        /// <returns>Number of files in the given project</returns>
         public int getNumberOfFilesInProject(int projectID)
         {
             return getProjectByID(projectID).numberOfFiles;
         }
+
         /// <summary>
-        /// This function deletes projects
+        /// This functions gets the type of a given project.
+        /// </summary>
+        /// <param name="projectID">ID of the project</param>
+        /// <returns>Type of project</returns>
+        public projectType getProjectType(int projectID)
+        {
+            return getProjectByID(projectID).type;
+        }
+
+        /// <summary>
+        /// This function deletes a given project and also all of its files.
         /// </summary>
         /// <param name="projectID"></param>
-        /// <returns>Returns the projectID</returns>
         public void deleteProject(int projectID)
         {
             var fileList = getFiles(projectID);
+
+            /* Delete all files in the project */
             foreach(File x in fileList)
             {
                 deleteFile(x.fileID);
             }
+
+            /*
+             * Todo: Remove all links between the users of the project and the project
+             */
+
             db.Projects.Remove(getProjectByID(projectID));
             db.SaveChanges();
         }
+
+        #endregion
+
+        #region File functions
+
         /// <summary>
-        /// This function creates a new file in a given project
+        /// This function creates a new file in a given project.
         /// </summary>
         /// <param name="projectID"></param>
+        /// <param name="userID"></param>
         /// <param name="fileName"></param>
-        /// <returns>fileID of file created</returns>
+        /// <returns>ID of the file created</returns>
         public int createFile(int projectID, int userID, string fileName)
         {
             File newFile = new File(fileName, getProjectType(projectID).ToString(), projectID);
-            incrementProjectFileCounter(projectID);
             Event newEvent = new Event(userID, newFile.fileID, DateTime.Now, Event.eventType.created);
+
+            incrementProjectFileCounter(projectID);
+
             db.Files.Add(newFile);
             db.Events.Add(newEvent);
             db.SaveChanges();
+
             return newFile.fileID;
         }
+
         /// <summary>
-        /// 
+        /// This functions gets a given file by id.
         /// </summary>
         /// <param name="fileID"></param>
-        /// <returns></returns>
+        /// <returns>A single file</returns>
         public File getFileByID(int fileID)
         {
             var f = (from x in db.Files
                      where x.fileID.Equals(fileID)
                      select x).SingleOrDefault();
+
             if(f == null)
             {
-                /* Todo exception */
+                /* Todo exception if búbú */
             }
+
             return f;
         }
+
         /// <summary>
-        /// This function deletes a single file from a given project
+        /// This function gets a file list for a given project.
         /// </summary>
-        /// <param name="fileID"></param>
-        /// <returns>fileID of the file deleted</returns>
-        public void deleteFile(int fileID)
-        {
-            decrementProjectFileCounter(getFileByID(fileID).projectID);
-            db.Files.Remove(getFileByID(fileID));
-            db.SaveChanges();
-        }
-
-
-
+        /// <param name="projectID"></param>
+        /// <returns>List of files</returns>
         public List<File> getFiles(int projectID)
         {
             List<File> f = (from x in db.Files
                             where x.projectID.Equals(projectID)
                             select x).ToList();
+
             return f;
         }
+
+        /// <summary>
+        /// This function deletes a single file from a given project
+        /// and also the event logs associated with that file.
+        /// </summary>
+        /// <param name="fileID"></param>
+        public void deleteFile(int fileID)
+        {
+            var l = getEventLog(fileID);
+
+            /* Remove all logs associated with that file */
+            foreach(Event x in l)
+            {
+                db.Events.Remove(x);
+            }
+
+            decrementProjectFileCounter(getFileByID(fileID).projectID);
+
+            db.Files.Remove(getFileByID(fileID));
+            db.SaveChanges();
+        }
+
+        #endregion
+
+        #region Event functions
+
+        /// <summary>
+        /// This function logs an event associated with a file.
+        /// </summary>
+        /// <param name="userID">ID of the user that made changes</param>
+        /// <param name="fileID">ID of the file beeing changed</param>
         public void logEvent(int userID, int fileID)
         {
             Event e = new Event(userID, fileID, DateTime.Now, Event.eventType.modified);
+
             db.Events.Add(e);
             db.SaveChanges();
         }
+
+        /// <summary>
+        /// This function return a list of events associated with a given file.
+        /// </summary>
+        /// <param name="fileID">ID of the file associated with the event logs</param>
+        /// <returns>List of events</returns>
         public List<Event> getEventLog(int fileID)
         {
             List<Event> e = (from x in db.Events
                              where x.fileID.Equals(fileID)
                              select x).ToList();
+
             return e;
         }
 
+        #endregion
 
-        /*
-         * The following functions are private helper functions 
-         * for the other functions in ProjectServices.
-         */
-         /// <summary>
-         /// 
-         /// </summary>
-         /// <param name="projectID"></param>
-         /// <returns></returns>
-        private projectType getProjectType(int projectID)
-        {
-            return getProjectByID(projectID).type;
-        }
+        #region Helper functions
+
         /// <summary>
-        /// 
+        /// This function increments the project file counter for a given project.
         /// </summary>
         /// <param name="projectID"></param>
         private void incrementProjectFileCounter(int projectID)
         {
             Project p = getProjectByID(projectID);
+
             p.numberOfFiles++;
+
             db.SaveChanges();
         }
 
+        /// <summary>
+        /// This function decrements the project file counter for a given project.
+        /// </summary>
+        /// <param name="projectID"></param>
         private void decrementProjectFileCounter(int projectID)
         {
             Project p = getProjectByID(projectID);
+
             p.numberOfFiles--;
+
             db.SaveChanges();
         }
+
+        #endregion
 
     }
 }
