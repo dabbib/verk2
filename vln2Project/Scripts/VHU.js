ed60e650bfadf49e3860b9a872800f5af16bb5d9
agustha16 <agustha16@ru.is>
2017-05-08 03:07:30 +0000

    Changed the look of the registration page, login page, and the website as a whole. Not final design.

---
+++ vln2Project/Scripts/VHU.js
@@ -0,0 +1,8 @@
+﻿var $output = $('#output');
+$(window).on('scroll', function () {
+    var scrollTop = $(window).scrollTop(),
+        elementOffset = $('#foo').offset().top,
+        distance = (elementOffset - scrollTop);
+
+    $output.prepend('<p>' + distance + '</p>');
+});
