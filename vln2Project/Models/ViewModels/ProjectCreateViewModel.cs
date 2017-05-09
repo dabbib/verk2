d81687a74a5d759b3fdfe21b55459dd9b8f67985
orria16 <orria16@ru.is>
2017-05-09 12:59:10 +0000

    create view model projectCreateViewModel

---
+++ vln2Project/Models/ViewModels/ProjectCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace h37.Models.ViewModels
+{
+    public class ProjectCreateViewModel
+    {
+        public string projectName { get; set; }
+
+        public int projectType { get; set; }
+    }
+}
