ce661b501c2f8b43e1890bb5e8e5776b50171baf
helgib10 <helgib10@ru.is>
2017-05-12 16:16:26 +0000

    unit test trial

---
+++ vln2Project.Tests/Services/projectServicesTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using h37.Models.Entities;
+using h37.Models.ViewModel;
+
+using h37.Services;
+namespace vln2Project.Tests.servicesTest
+{
+    [TestClass]
+    public class projectsServicestest
+    {
+        [TestMethod]
+        public void test()
+        {
+            //Arrange
+           
+            //Act
+
+            //Assert
+
+        }
+
+
+
+    }
+}
